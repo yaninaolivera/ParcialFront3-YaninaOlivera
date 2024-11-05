466b1fa72f71935e104389141f907371f1042cc7
yaninaoliverav <yaninaoliverav@gmail.com>
2024-11-05 11:25:16 -0500

    Inicio proyecto en React

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Card from './Components/Card'
+
+function App() {
+  return (
+    <>
+      <div className='App'>
+        <h1>Carga de estudiantes</h1>
+        <form></form>
+        <Card />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Card.jsx
@@ -0,0 +1,9 @@
+const Card = () => {
+  return (
+    <div>
+      Hola, soy una tarjeta
+    </div>
+  )
+}
+
+export default Card
